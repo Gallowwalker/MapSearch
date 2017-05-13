15a285a738910a45a7b6c3a99c48364d3a51c213
Gallowwalker <naka3a7el@abv.bg>
2017-05-13 22:37:00 +0300

    New Update.

--- src/map/GraphUtil.java
+++ src/map/GraphUtil.java
@@ -3,18 +3,19 @@ package map;
 public class GraphUtil {
 	
 	public static double calculatePathCost(byte linkType, double linkLength) {
+		double cost = 0.0d;
+		
 		switch (linkType) {
 			case 0:
 				break;
 			case 1:
 				break;
 			case 2: return 0.0d;
-			default:
+			default: System.out.println("error"); // implement error dialog 
 				break;
 		}
 		
-		
-		return 0.0d;
+		return cost;
 	}
 
 }
