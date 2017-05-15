0e8d38ca381f8d5861411c9a46c5d89fea12dc72
Gallowwalker <naka3a7el@abv.bg>
2017-05-15 15:00:05 +0300

    Path cost calculations added.

--- src/map/GraphUtil.java
+++ src/map/GraphUtil.java
@@ -7,8 +7,20 @@ public class GraphUtil {
 		
 		switch (linkType) {
 			case 0:
+				cost += 3;
+				if (linkLength > 10) {
+					linkLength -=10;
+				}
+				cost += linkLength * 0.5d;
 				break;
 			case 1:
+				linkLength -= 5;
+				linkLength /= 10;
+				if (linkLength != Math.round(linkLength)) {
+					linkLength += 1;
+					linkLength = Math.floor(linkLength);
+				}
+				cost = linkLength * 2.0d;
 				break;
 			case 2: return 0.0d;
 			default: System.out.println("error"); // implement error dialog 
