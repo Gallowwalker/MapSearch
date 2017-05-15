bb326d9b8ef4db2fe4a2eddfa49c6597ecef4bb9
Gallowwalker <naka3a7el@abv.bg>
2017-05-15 22:42:57 +0300

    UI update.

--- src/gui/UserInterface.java
+++ src/gui/UserInterface.java
@@ -5,14 +5,16 @@ import javax.swing.JFrame;
 import processors.DialogProcessor;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class UserInterface {
 
 	private JFrame mainWindow = null;
 	private JPanel buttonsPanel = null;
-	private JButton newMapButton = null;
-	private JButton loadMapButton = null;
-	private JButton saveMapButton = null;
+	private JButton newMapButton = null, loadMapButton = null, saveMapButton = null;
+	private JButton dijkstraSearchButton = null, greedySearchButton = null, customSearchButton = null;
+	private JButton resetButton = null, exitButton = null;
+	private JLabel optionsLabel = null, algoritmLabel = null, otherLabel = null;
 	
 	private DialogProcessor dialogProcessor = new DialogProcessor();
 	private DisplayArea displayArea = new DisplayArea(this);
@@ -29,27 +31,57 @@ public class UserInterface {
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		buttonsPanel = new JPanel();
-		buttonsPanel.setBounds(0, 0, 199, 921);
+		buttonsPanel.setBounds(0, 0, 149, 921);
 		buttonsPanel.setLayout(null);
 		
 		mainWindow.getContentPane().add(buttonsPanel);
 		
+		optionsLabel = new JLabel("Options");
+		optionsLabel.setBounds(50, 11, 129, 28);
+		
 		newMapButton = new JButton("New Map");
-		newMapButton.setBounds(10, 11, 89, 23);
+		newMapButton.setBounds(10, 44, 129, 28);
 		
 		loadMapButton = new JButton("Load Map");
-		loadMapButton.setBounds(10, 44, 89, 23);
+		loadMapButton.setBounds(10, 77, 129, 28);
 		
 		saveMapButton = new JButton("Save Map");
-		saveMapButton.setBounds(10, 77, 89, 23);
+		saveMapButton.setBounds(10, 110, 129, 28);
 		
+		algoritmLabel = new JLabel("Algoritms");
+		algoritmLabel.setBounds(50, 176, 129, 28);
+		
+		dijkstraSearchButton = new JButton("Implement Dijkstra");
+		dijkstraSearchButton.setBounds(10, 209, 129, 28);
+		
+		greedySearchButton = new JButton("Implement Greedy");
+		greedySearchButton.setBounds(10, 242, 129, 28);
+		
+		customSearchButton = new JButton("Implement Other");
+		customSearchButton.setBounds(10, 275, 129, 28);
+		
+		otherLabel = new JLabel("Other");
+		otherLabel.setBounds(60, 341, 129, 28);
+		
+		resetButton = new JButton("Reset Search");
+		resetButton.setBounds(10, 374, 129, 28);
+		
+		exitButton = new JButton("Exit Program");
+		exitButton.setBounds(10, 407, 129, 28);
+		
+		buttonsPanel.add(optionsLabel);
 		buttonsPanel.add(newMapButton);
 		buttonsPanel.add(loadMapButton);
 		buttonsPanel.add(saveMapButton);
+		buttonsPanel.add(algoritmLabel);
+		buttonsPanel.add(dijkstraSearchButton);
+		buttonsPanel.add(greedySearchButton);
+		buttonsPanel.add(customSearchButton);
+		buttonsPanel.add(otherLabel);
+		buttonsPanel.add(resetButton);
+		buttonsPanel.add(exitButton);
 		
-
-		
-		displayArea.setBounds(199, 0, 1475, 921);
+		displayArea.setBounds(149, 0, 1525, 921);
 		mainWindow.getContentPane().add(displayArea);
 		
 		mainWindow.setVisible(true);
